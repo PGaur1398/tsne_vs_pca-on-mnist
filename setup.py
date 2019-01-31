320607783335ddd81774c33dda40b237dcfb1e6f
PGaur98 <pankajs98@outlook.com>
2019-01-31 18:27:50 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+name = "tsne_vs_pca",
+version = "0.01",
+description  = "Comparision between two dimensionality reduction algorithm",
+author = "Pankaj Gaur",
+author_email = "pankajs98@outlook.com",
+py_modules = ["tsne_vs_pca"],
+
+  )
