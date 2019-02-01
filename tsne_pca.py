8162fe68d5e0513cbc92d33e84dc8607e7c785e1
PGaur1398 <pankajsa2z98@gmail.com>
2019-02-01 13:31:57 +0530

    ..

--- tsne_pca.py
+++ tsne_pca.py
@@ -11,7 +11,7 @@ class tsne_pca:
     def plot_tsne(self,csv_file):
         import seaborn as sn
         import matplotlib.pyplot as plt
-        from tsne._tsne import tsne
+        from _tsne import tsne
         sn.FacetGrid(tsne(csv_file), hue="label", size=6).map(plt.scatter, 'Dim_1', 'Dim_2').add_legend()
         plt.title("T-SNE")
         plt.show()
